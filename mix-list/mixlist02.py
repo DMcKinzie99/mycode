a386c066e880e6f2c08396697df2791a83da2816
DMcKinzie99 <DMcKinzie99@users.noreply.github.com>
2022-04-18 21:05:46 +0000

    mixed lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#New List
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("ipaddresses:", iplist[3], iplist[4])
+
+
+
+
+
---
+++ script1.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+#This is the input asking the users name
+name= input("What is your name?")
+print("Good afternoon so your name is " + name)
+dayoftheweek= input("What day is it today?")
+print("Well Happy " + dayoftheweek, "! Hope it goes well " +name,"!")
