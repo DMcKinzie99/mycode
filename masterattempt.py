4a03c25bb078e826df25c66542a44c920cd8831a
DMcKinzie99 <DMcKinzie99@users.noreply.github.com>
2022-04-19 16:38:03 +0000

    Learning about dictionaries

---
+++ masterattempt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#Making a List
+
+icecream= ["indentation", "spaces"] 
+
+tlgstudents= ["Akino", "Bai", "Carlos", "Dalton", "Dan", "Edith", "Ethan", "Isaiah", "J", "Jessica", "John", "Justin", "Khalil", "Nikk", "Ramesh", "Scotty", "Sergio", "Shawn"]
+
+icecream.append(4)
+print(icecream)
+
+choice= int(input("Can you tell me a number between 0 and 14: "))
+#Lets hope that worked.
+student_name= tlgstudents[choice]
+print("You picked the student " +student_name)
+int(student_name)
+print(+student_name, "always uses")
+
+
+
