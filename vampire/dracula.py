5184119b3f9f2e278d4fa5e98ac033d8538383ba
DMcKinzie99 <DMcKinzie99@users.noreply.github.com>
2022-04-22 15:29:05 +0000

    Warmup vampire

---
+++ vampire/dracula.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+count = 0
+zoink= open("vampytimes.txt", "w")
+with open("345-0.txt","r") as foo:
+    for line in foo:
+        if "vampire" in line.lower():
+            print(line)
+            count += 1
+            zoink.write(line)
+
+print(count)
+
+
